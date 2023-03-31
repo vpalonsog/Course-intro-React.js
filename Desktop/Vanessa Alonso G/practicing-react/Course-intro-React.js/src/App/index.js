541a792e6dd1ec48604af5b31865d1fa0f3f7a6e
Vanessa <vpalonsog@gmail.com>
2023-03-31 14:02:45 -0500

    [NEW BRANCH] add probing branch

--- Desktop/Vanessa Alonso G/practicing-react/Course-intro-React.js/src/App/index.js
+++ Desktop/Vanessa Alonso G/practicing-react/Course-intro-React.js/src/App/index.js
@@ -1,13 +1,8 @@
 import React from "react";
-import { TodoProvider } from "../TodoContext";
 import { AppUI } from "./AppUI";
 
 function App() {
-  return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
-  );
+  return <AppUI />;
 }
 
 export default App;
